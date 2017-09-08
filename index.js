69e7ff0c12ffff73e535b9872928feea46f993d8
rlrh1996 <rlrh1996@gmail.com>
2017-09-08 08:40:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,13 @@ function logShout(string) {
 function logWhisper(string) {
     console.log(whisper(string))
 }
+
+function sayHiToGrandma(string) {
+    if (string === whisper(string)) {
+        return "I can\'t hear you!"
+    } else if (string === shout(string)) {
+        return "YES INDEED!"
+    } else if (string === "I love you, Grandma.") {
+        return "I love you, too"
+    }
+}
